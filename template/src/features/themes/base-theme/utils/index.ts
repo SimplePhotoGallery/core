7203d43dac81112a321191d7c08828d86b72dc5a
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-08-05 00:03:03 +0200

    fix: link to subgallery

--- template/src/features/themes/base-theme/utils/index.ts
+++ template/src/features/themes/base-theme/utils/index.ts
@@ -1,18 +1,18 @@
 import path from 'node:path';
 
 /**
- * Normalizes image paths to be relative to the gallery root directory instead of the gallery.json file.
+ * Normalizes resource paths to be relative to the gallery root directory instead of the gallery.json file.
  *
- * @param imagePath - The image path, typically relative to the gallery.json file
+ * @param resourcePath - The resource path (file or directory), typically relative to the gallery.json file
  * @returns The normalized path relative to the gallery root directory
  */
 
-export const getImagePath = (imagePath: string) => {
+export const getGalleryPath = (resourcePath: string) => {
   const galleryConfigPath = path.resolve(process.env.GALLERY_JSON_PATH || '');
   const galleryConfigDir = path.dirname(galleryConfigPath);
 
-  const absoluteImagePath = path.resolve(path.join(galleryConfigDir, imagePath));
+  const absoluteResourcePath = path.resolve(path.join(galleryConfigDir, resourcePath));
   const baseDir = path.dirname(galleryConfigDir);
 
-  return path.relative(baseDir, absoluteImagePath);
+  return path.relative(baseDir, absoluteResourcePath);
 };
