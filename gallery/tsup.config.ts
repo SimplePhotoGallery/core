6d87508e0d3c3e5cf6946ad1ffb4c776bbf4d346
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-10-28 13:45:37 +0100

    feat: export types and functions to be used by other libs

--- gallery/tsup.config.ts
+++ gallery/tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
+  entry: ['src/index.ts', 'src/lib/index.ts'],
+  format: ['esm', 'cjs'],
   dts: true,
   splitting: false,
   sourcemap: true,
